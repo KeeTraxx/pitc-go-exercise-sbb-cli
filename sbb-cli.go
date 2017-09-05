5604fb486c9a2e1b753aa079d84875254854b63e
Khôi Tran <tran@puzzle.ch>
2017-09-05 11:11:18 +0200

    initial commit

---
+++ sbb-cli.go
@@ -0,0 +1,17 @@
+package main
+
+func main() {
+
+	// TODO: use http.Get to make HTTP Requests to SBB API
+	// docs:
+	// http://transport.opendata.ch/docs.html#connections
+
+	// TODO: Read the body of the HTTP Request
+	// Hint: use ioutil.ReadAll
+
+	// TODO: Convert JSON response to struct
+	// Hint: Use json.Unmarshal
+
+	// TODO: Loop through all connections and print them
+
+}
